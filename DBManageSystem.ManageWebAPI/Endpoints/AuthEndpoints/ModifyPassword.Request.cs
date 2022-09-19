86a1a0e38049b635b27f64d3edd72ace00e0e37c
MHarry <nokia18819479253@outlook.com>
2022-09-19 23:48:02 +0800

    add modifypassword endpoint

---
+++ DBManageSystem.ManageWebAPI/Endpoints/AuthEndpoints/ModifyPassword.Request.cs
@@ -0,0 +1,12 @@
+﻿namespace DBManageSystem.ManageWebAPI.Endpoints.AuthEndpoints
+{
+    public class ModifyPasswordRequest
+    {
+        public const string Route = "/users/password";
+        public int UserId { get; set; }
+
+        public string OldPassword { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
---
+++ DBManageSystem.ManageWebAPI/Endpoints/AuthEndpoints/ModifyPassword.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBManageSystem.ManageWebAPI.Endpoints.AuthEndpoints
+{
+    public class ModifyPassword : EndpointBaseAsync.WithRequest<ModifyPasswordRequest>.WithActionResult
+    {
+        public override Task<ActionResult> HandleAsync(ModifyPasswordRequest request, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
