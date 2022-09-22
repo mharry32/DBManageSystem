8b8e04682c0ea3b285a434318eb03fab2b2d2428
MHarry <nokia18819479253@outlook.com>
2022-09-22 15:59:01 +0800

    o

---
+++ DBManageSystem/tests/DBManageSystem.FunctionalTests/ApiEndpoints/RoleOpFunctionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBManageSystem.ManageWebAPI;
+using Xunit;
+
+namespace DBManageSystem.FunctionalTests.ApiEndpoints;
+public class RoleOpFunctionTests:IClassFixture<FunctionalTestWebApplicationFactory<ManageWebMarker>>
+{
+  private readonly HttpClient _client;
+
+  private readonly string token;
+
+  public RoleOpFunctionTests(FunctionalTestWebApplicationFactory<ManageWebMarker> factory)
+  {
+    _client = factory.CreateClient();
+    var tokenTask = factory.GetAuthToken(_client);
+    tokenTask.Wait();
+    token = tokenTask.Result;
+  }
+
+  [Fact]
+  public async Task BasicRoleFunctionTest()
+  {
+     
+  }
+}
