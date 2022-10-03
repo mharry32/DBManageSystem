310129b8405cd4edbb147a9cde53b0ff6d95f5cb
mharry <nokia18819479253@outlook.com>
2022-10-03 17:59:43 +0800

    add some properties for entity

--- DBManageSystem/src/DBManageSystem.Core/Entities/DatabaseServer.cs
+++ DBManageSystem/src/DBManageSystem.Core/Entities/DatabaseServer.cs
@@ -10,11 +10,14 @@ using DBManageSystem.SharedKernel;
 namespace DBManageSystem.Core.Entities;
 public class DatabaseServer:EntityBase
 {
+  public string Name { get; set; }
   public string ConnectUrl { get; set; }
 
   public string? UserName { get; set; }
 
   public string? Password { get; set; }
+
+  public DateTime? LastUpdatedTime { get; set; }
 
   public DatabaseStatusEnum Status { get; set; }
 }
---
+++ DBManageSystem/src/DBManageSystem.Core/Entities/SqlLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBManageSystem.SharedKernel;
+
+namespace DBManageSystem.Core.Entities;
+public class SqlLog:EntityBase
+{
+  public string ConnectUrl { get; set; }
+
+  public DateTime ExecuteTime { get; set; }
+
+  public string SqlText { get; set; }
+}
