64f81aa896626627fcc3f0bca58957925ef51b99
MHarry <nokia18819479253@outlook.com>
2022-10-11 22:53:43 +0800

    add execute sql api

---
+++ DBManageSystem.ManageWebAPI/Endpoints/DBOperationsEndpoints/ExecuteSql.Request.cs
@@ -0,0 +1,10 @@
+﻿namespace DBManageSystem.ManageWebAPI.Endpoints.DBOperationsEndpoints
+{
+    public class ExecuteSqlRequest
+    {
+        public int ServerId { get; set; }
+        public string DatabaseName { get; set; }
+
+        public string SqlText { get; set; }
+    }
+}
---
+++ DBManageSystem.ManageWebAPI/Endpoints/DBOperationsEndpoints/ExecuteSql.cs
@@ -0,0 +1,44 @@
+﻿using Ardalis.ApiEndpoints;
+using AutoMapper;
+using DBManageSystem.Core.Entities;
+using DBManageSystem.Core.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBManageSystem.ManageWebAPI.Endpoints.DBOperationsEndpoints
+{
+    public class ExecuteSql : EndpointBaseAsync.WithRequest<ExecuteSqlRequest>.WithActionResult<SqlExecuteResult>
+    {
+        private readonly IMapper _mapper;
+        private readonly IDbServiceStrategy _dbServiceStrategy;
+        private readonly IDatabaseServerService _databaseServerService;
+        public ExecuteSql(IDbServiceStrategy strategy, IDatabaseServerService manageService, IMapper mapper)
+        {
+            _mapper = mapper;
+            _dbServiceStrategy = strategy;
+            _databaseServerService = manageService;
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost("/dbschema/execsql")]
+        public override async Task<ActionResult<SqlExecuteResult>> HandleAsync(ExecuteSqlRequest request, CancellationToken cancellationToken = default)
+        {
+            var dbserverResult = await _databaseServerService.GetDatabaseServerWithPasswordDecrptedById(request.ServerId);
+            if(dbserverResult.IsSuccess == false)
+            {
+                return new UnprocessableEntityResult();
+            }
+
+            var dbserver = dbserverResult.Value;
+
+            var service = _dbServiceStrategy.Decide(dbserver.DatabaseType);
+
+            var execResult = await service.ExecuteSql(dbserver, request.DatabaseName, request.SqlText);
+
+            return execResult;
+        }
+    }
+}
