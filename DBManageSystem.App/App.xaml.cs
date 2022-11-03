a9c2efba0e8be319acc0b3de3adddb1b1da31f1b
mharry <nokia18819479253@outlook.com>
2022-11-03 22:55:14 +0800

    add maui app

---
+++ DBManageSystem.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DBManageSystem.App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ DBManageSystem.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DBManageSystem.App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
